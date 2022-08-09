b96e709ef5256f2430f6dc2e4bb708a192961681
Nikita <nikitapohod@icloud.com>
2022-08-09 21:27:37 +0300

    Fix 01

--- Seminar_2/01_IsNumAMultipleOf_7_And_23/Program.cs
+++ Seminar_2/01_IsNumAMultipleOf_7_And_23/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите число: ");
+﻿// 1. Дано число. Проверить кратно ли оно 7 и 23.
+
+Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 if ((num%23 == 0) && (num%7 == 0)) Console.WriteLine($"Число {num} кратно 23 и 7!");
 else if (num%23 == 0) Console.WriteLine($"Число {num} кратно только 23!");
