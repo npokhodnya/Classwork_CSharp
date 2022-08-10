50424689754eeb35ddb94bf32274a5f13322bec1
Nikita <nikitapohod@icloud.com>
2022-08-10 15:04:49 +0300

    Task 03 done

---
+++ Seminar_2/03_IsNumPalindrome/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число для проверки на палиндром: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 9999 && num < 100000)
+{   
+    int palindromeNum = (num%10*10000)+((num%100-num%10)*100)+(num%1000-num%100)+((num%10000-num%1000)/100)+(num/10000);
+    if (num == palindromeNum) Console.WriteLine($"Число {num} является палиндромом! ({num} = {palindromeNum})!");
+    else Console.WriteLine($"Число {num} НЕ является палиндромом! ({num} != {palindromeNum})!");
+}
+else
+{
+    Console.WriteLine($"Число должно быть пятизначным!");
+}
