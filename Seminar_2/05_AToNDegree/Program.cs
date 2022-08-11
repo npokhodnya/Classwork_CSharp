6b7fa9319146850997af9aee1fa9dc59ea8a2309
Nikita <nikitapohod@icloud.com>
2022-08-11 14:25:20 +0300

    Task 05 added function

--- Seminar_2/05_AToNDegree/Program.cs
+++ Seminar_2/05_AToNDegree/Program.cs
@@ -1,15 +1,19 @@
 ﻿// Возведите число А в натуральную степень B используя цикл.
 
+int Degree(int num, int degree)
+{
+    int count = 1;
+    int res = num;
+    while (count<degree)
+    {
+        res*=num; 
+        count++;
+    }
+    return res;
+}
+
 Console.Write("Введите число для возведения в степень: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите показатель степени: ");
 int n = int.Parse(Console.ReadLine());
-int count = 1;
-int res = a;
-while (count<n)
-{
-    res*=a; 
-    count++;
-}
-    
-Console.WriteLine($"{a}^{n} = {res}");
+Console.WriteLine($"{a}^{n} = {Degree(a, n)}");
