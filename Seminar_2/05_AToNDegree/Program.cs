6e12b38ce3e188dbd245142c302952aebe27f9cd
Nikita <nikitapohod@icloud.com>
2022-08-11 14:10:12 +0300

    Task 05 done

---
+++ Seminar_2/05_AToNDegree/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+
+Console.Write("Введите число для возведения в степень: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите показатель степени: ");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+int res = a;
+while (count<n)
+{
+    res*=a; 
+    count++;
+}
+    
+Console.WriteLine($"{a}^{n} = {res}");
