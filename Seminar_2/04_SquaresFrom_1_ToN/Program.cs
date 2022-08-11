880e6be2db7b5e9f57d381bd9b503a03c3f8bce4
Nikita <nikitapohod@icloud.com>
2022-08-11 13:41:18 +0300

    Task 04 done

---
+++ Seminar_2/04_SquaresFrom_1_ToN/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N.
+
+Console.Write("Ведите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+while(count<=num)
+{
+    Console.WriteLine(count*count);
+    count++;
+}
