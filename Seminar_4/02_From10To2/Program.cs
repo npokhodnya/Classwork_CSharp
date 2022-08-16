49b3c76a421c67f6e65521e56af960f12715bff7
Nikita <nikitapohod@icloud.com>
2022-08-16 21:55:30 +0300

    Task 02 completed

---
+++ Seminar_4/02_From10To2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+
+int From_10_To_2(int num)
+{
+    int binary = 0;
+    for (int i = 0; num>=1; i++)
+    {
+        binary += num % 2 * (int)Math.Pow(10, i);
+        num/=2;
+    }
+    return binary;
+}
+Console.Clear();
+Console.Write("Введите число в десятичной сс: ");
+var num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Результат: {From_10_To_2(num)}");
