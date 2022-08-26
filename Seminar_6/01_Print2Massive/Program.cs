e7c64a8971de3b3887a3a508927f137f6de1f150
Nikita <nikitapohod@icloud.com>
2022-08-26 18:00:03 +0300

    Tasks of Seminar_6 all sloved

---
+++ Seminar_6/01_Print2Massive/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+
+void FillMatrix(int[,] array, int min, int max)
+{
+    var rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) array[i, j] = rand.Next(min, max+1);
+    }
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите число столбцов: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Введите число сторк: ");
+var m = int.Parse(Console.ReadLine());
+int[,] massive = new int[m, n];
+FillMatrix(massive, 0, 9);
+PrintMatrix(massive);
---
+++ Seminar_6/02_ChangeElementOfMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+
+void FillMatrix(int[,] array, int min, int max)
+{
+    var rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) array[i, j] = rand.Next(min, max+1);
+    }
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+void ChangeElementMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            if(array[i, j]%2 == 0) array[i, j] = -array[i, j];
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите число столбцов: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Введите число сторк: ");
+var m = int.Parse(Console.ReadLine());
+int[,] massive = new int[m, n];
+FillMatrix(massive, 0, 9);
+Console.WriteLine("До изменений: ");
+PrintMatrix(massive);
+Console.WriteLine();
+Console.WriteLine("После изменений: ");
+ChangeElementMatrix(massive);
+PrintMatrix(massive);
