c0754064d060c3744b18b18c2877e2b7e6eb925c
Nikita <nikitapohod@icloud.com>
2022-08-30 11:59:20 +0300

    Task 01 sloved

---
+++ Seminar_8/01_SumDiogonal/Program.cs
@@ -0,0 +1,37 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали.
+
+void FillMatrix(int[,] array, int min, int max)
+{
+    var rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) array[i, j] = rand.Next(min, max+1);
+    }
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int CheckSumDiagonal(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++) 
+        sum += array[i, i];
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число столбцов: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Введите число строк: ");
+var m = int.Parse(Console.ReadLine());
+int[,] massive = new int[m, n];
+FillMatrix(massive, 0, 9);
+Console.WriteLine();
+Console.WriteLine("Матрица: ");
+PrintMatrix(massive);
+Console.WriteLine();
+Console.WriteLine($"Ответ: {CheckSumDiagonal(massive)}");
